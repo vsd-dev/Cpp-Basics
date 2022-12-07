0303cccce173747cdabdab810d901369b78fd3ca
VishnuDev-droid <vishnuthampan2k14@gmail.com>
2022-12-07 09:40:08 -0800

    character array

--- pointer/char_array.cpp
+++ pointer/char_array.cpp
@@ -1,13 +1,25 @@
 #include <iostream>
 #include <string.h>
 
-int main(){
+void print(char C[])
+{
+    int i = 0;
+    while (C[i] != '\0')
+    {
+        printf("%c ", C[i]);
+        i++;
+    }
+    printf("\n");
+}
+int main()
+{
 
     char C[] = "JOHN";
-     
-    printf("%s \n", C); //Null termination (Not needed for cpp)
+    printf("%s \n", C);                          // Null termination (Not needed for cpp)
     printf("Size in bytes = %lu \n", sizeof(C)); // %lu for unsigned long datatype
     int len = strlen(C);
     printf("Length of string = %d \n", len); // null termminated char not included
+
+    print(C);
     return 0;
 }
