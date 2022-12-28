04fa0501ecfdf11755aa50c92327a5f284b47b95
VishnuDev-droid <vishnuthampan2k14@gmail.com>
2022-12-27 16:36:16 -0800

    malloc, calloc

--- dynamic_memory_alloc/main.c
+++ dynamic_memory_alloc/main.c
@@ -1,10 +1,29 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 
-int main(){
-    int a; // goes on stack
-    int *p;//Defines pointer
-    p = (int*) malloc(sizeof(int));
-    *p = 10;
+int main()
+{
+    int n;
+    printf("Enter size of array \n");
+    scanf("%d", &n);
+    // int *A = (int*)malloc(n * sizeof(int));
+
+    int *A = (int*)calloc(n, sizeof(int));
+
+
+    for (int i=0;i<n;i++)
+    {
+        A[i] = i + 1;
+    }
+
+    for (int i=0;i<n;i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+
+    free(A);
+
     return 0;
 }
